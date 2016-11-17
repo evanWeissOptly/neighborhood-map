28c0139c0b13da5c8f5c47d7de606e025c067e69
Evan Weiss <evan.weiss@optimizely.com>
2016-11-17 10:54:46 -0800

    fixed object naming and quotes in app.js

--- js/app.js
+++ js/app.js
@@ -1,66 +1,67 @@
-// define our Retaurant model and default values
+// define our restaurant default values
 var Model = {
     restaurants: [
         {
-            name: "Broderick Roadhouse",
-            description: "Massive, fresh burgers and a great selection of craft beer.",
+            name: 'Broderick Roadhouse',
+            description: 'Massive, fresh burgers and a great selection of craft beer.',
             position: {lat: 37.900009, lng: -122.062241}
         },
         {
-            name: "Mixed Grain",
-            description: "The best korean in Walnut Creek.  Try the spicy pork!",
+            name: 'Mixed Grain',
+            description: 'The best korean in Walnut Creek.  Try the spicy pork!',
             position: {lat:37.900062, lng: -122.062043}
         },
         {
-            name: "Brass Bear",
-            description: "Solid deli sandwiches!",
+            name: 'Brass Bear',
+            description: 'Solid deli sandwiches!',
             position: {lat: 37.900514, lng: -122.061540}
         },
         {
-            name: "Lottie's Creamery",
-            description: "Amazing ice cream.  Get a waffle cone!",
+            name: 'Lottie\'s Creamery',
+            description: 'Amazing ice cream.  Get a waffle cone!',
             position: {lat: 37.899807, lng: -122.060686}
         },
         {
-            name: "Dragon's Pond",
-            description: "Solid chinese food.  They deliver!",
+            name: 'Dragon\'s Pond',
+            description: 'Solid chinese food.  They deliver!',
             position: {lat: 37.898500, lng: -122.062014}
         }
-    ],
-    /* address, category, phone and URL will be populated by async Foursquare API
-    and therefore are defined as observables (because we want to be notified when)
-    the API call completes and they are updted.  All other properties don't require
-    notification */
-    Restaurant: function(data) {
-        this.name = data.name;
-        this.description = data.description;
-        this.position = data.position;
-        this.infowindow;
-        this.marker;
-        this.address = ko.observable();
-        this.category = ko.observable();
-        this.phone = ko.observable();
-        this.url = ko.observable();
-    }
+    ]
+};
+
+/* address, category, phone and URL will be populated by async Foursquare API
+and therefore are defined as observables (because we want to be notified when)
+the API call completes and they are updted.  All other properties don't require
+notification */
+var Restaurant = function(data) {
+    this.name = data.name;
+    this.description = data.description;
+    this.position = data.position;
+    this.infowindow;
+    this.marker;
+    this.address = ko.observable();
+    this.category = ko.observable();
+    this.phone = ko.observable();
+    this.url = ko.observable();
 };
 
 // define viewmodel
-var viewModel = function() {
+var ViewModel = function() {
     var self = this;
 
     /* restaurantList will hold all restaurant objects,
     and filteredList will be the subset of restaurants matching the search keyword*/
     this.restaurantList = ko.observableArray();
     Model.restaurants.forEach(function(restaurant){
-        self.restaurantList().push( new Model.Restaurant(restaurant) );
+        self.restaurantList().push( new Restaurant(restaurant) );
     });
     this.filteredList = ko.observableArray(this.restaurantList());
 
     // jQuery event handler for filter form submit
-    $("#filter").submit(function(event){
+    $('#filter').submit(function(event){
         // prevent form submit and then get search keyword
         event.preventDefault();
-        var kw = $("#kw").val().toLowerCase();
+        var kw = $('#kw').val().toLowerCase();
         // reset filteredList
         self.filteredList([]);
 
@@ -81,15 +82,15 @@ var viewModel = function() {
 
     // function to populate infowindow content with Foursquare data
     this.populateContent = function(restaurant) {
-        var content = "<h4>" + restaurant.name + "</h4>";
-        content += "<h5>" + restaurant.category() + "</h5>";
+        var content = '<h4>' + restaurant.name + '</h4>';
+        content += '<h5>' + restaurant.category() + '</h5>';
         content += '<a href="' + restaurant.url() + '" target="_blank">' + restaurant.url() + '</a>'
-        content += "<div>" + restaurant.phone() + "</div>";
+        content += '<div>' + restaurant.phone() + '</div>';
         restaurant.address().forEach(function(line){ // note that address is an array
-            content += "<div>" + line + "</div>";
+            content += '<div>' + line + '</div>';
         });
         content += '<div class="disclaimer">Contact info courtesy of Foursquare</div>';
-        content += "Evan says: <i>" + restaurant.description + "</i>";
+        content += 'Evan says: <i>' + restaurant.description + '</i>';
 
         restaurant.infowindow.setContent(content);
     };
@@ -124,18 +125,18 @@ var viewModel = function() {
 
     // nav tray open and close (courtesy of http://www.w3schools.com/howto/howto_js_sidenav.asp)
     this.openNav = function() {
-        document.getElementById("sidenav").style.width = "250px";
+        document.getElementById('sidenav').style.width = '250px';
     };
 
     this.closeNav = function() {
-        document.getElementById("sidenav").style.width = "0";
+        document.getElementById('sidenav').style.width = '0';
     };
 
     // Build foursquare API request url, query will be the restaurant's name
     this.request_url = function(query) {
-        return "https://api.foursquare.com/v2/venues/search?near=walnut creek&query=" + query +
-        "&intent=match&client_id=WLTEWU4ZCJ0XA1Z0L2VQBUAXKQIGLUQGI1NBG4XV2M0ZLMND&" +
-        "client_secret=5RG21TJCKX3XQFRAE5BUDKKHTEZCAPQI5G0XZAJQITUZGU31&v=20161115";
+        return 'https://api.foursquare.com/v2/venues/search?near=walnut creek&query=' + query +
+        '&intent=match&client_id=WLTEWU4ZCJ0XA1Z0L2VQBUAXKQIGLUQGI1NBG4XV2M0ZLMND&' +
+        'client_secret=5RG21TJCKX3XQFRAE5BUDKKHTEZCAPQI5G0XZAJQITUZGU31&v=20161115';
     };
 
     // Get Foursquare data for each restaurant (we'll do address, phone number, category and url)
@@ -145,15 +146,15 @@ var viewModel = function() {
             success: function(data) {
                 // on success, grab the data from the response and set on the restaurant object
                 var venue = data.response.venues[0];
-                restaurant.address(venue.location.formattedAddress || "");
-                restaurant.phone(venue.contact.formattedPhone || "");
-                restaurant.category(venue.categories[0].name || "");
-                restaurant.url(venue.url || "");
+                restaurant.address(venue.location.formattedAddress || '');
+                restaurant.phone(venue.contact.formattedPhone || '');
+                restaurant.category(venue.categories[0].name || '');
+                restaurant.url(venue.url || '');
             },
             error: function() {
                 // on failure, populate self.error to be displayed in error div
-                var errorText = "A Foursquare API error occurred.  " +
-                "Restaurant contact information may not be available.";
+                var errorText = 'A Foursquare API error occurred.  ' +
+                'Restaurant contact information may not be available.';
                 if (self.error().indexOf(errorText) == -1) {
                     self.error.push(errorText);
                 }
@@ -164,7 +165,7 @@ var viewModel = function() {
 };
 
 // initialize viewmodel
-var viewmodel = new viewModel();
+var viewmodel = new ViewModel();
 ko.applyBindings(viewmodel);
 
 
